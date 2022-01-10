8708f2c6c5e9eadb9e43e034789261fc58d5b290
Sebastian <sfdstrandberg@gmail.com>
2022-01-10 23:54:11 +0200

    whoops

--- reset.sql
+++ reset.sql
@@ -5,12 +5,6 @@ DELETE FROM player_matchups;
 DELETE FROM global_matchups;
 DELETE FROM high_rated_matchups;
 DELETE FROM config;
-
-DROP TABLE player_names;
-CREATE VIRTUAL TABLE player_names USING fts5(
-    id,
-    name,
-);
-
+DELETE FROM player_names;
 
 INSERT INTO config VALUES(1635717600);
