f69a0dfe02cc1575613a9b35246b3ac956f1ccf3
S <sovgottgames@gmail.com>
2024-01-11 11:04:04 +0200

    version bump

--- src/requests.rs
+++ src/requests.rs
@@ -3,7 +3,7 @@ use std::sync::Arc;
 use steamworks::{Client, TicketForWebApiResponse};
 use tokio::sync::Mutex;
 
-const VERSION: &str = "0.2.6";
+const VERSION: &str = "0.2.7";
 const PLAYER_ID: &str = "230129212655563979";
 
 #[derive(Debug, Serialize, Deserialize)]
